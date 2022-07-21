f1250af2d58a4c6b764aa42e5e6f164c8c51ac2a
Johnny <stormyjmaster@gmail.com>
2022-07-20 22:06:15 -0700

    updated

---
+++ src/Projects.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function projects(props){
+    return(
+        <div>
+            <h1>Projects</h1>
+        </div>
+    )
+}
+
+export default projects;
---
+++ src/cv.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function cv(props){
+    return(
+        <div>
+            <h1>CV</h1>
+        </div>
+    )
+}
+
+export default cv;
